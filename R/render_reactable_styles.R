2523af6c934159dedb37a6452e9946e876f57643
ceisenhauer <eisenhauer.catherine@gmail.com>
2022-10-07 16:56:43 +0100

    done i think

--- R/render_reactable_styles.R
+++ R/render_reactable_styles.R
@@ -1,3 +1,15 @@
+#' Reactable bar cell
+#' 
+#' @description Adds bar styling to a reactable cell through embedded CSS.
+#' 
+#' @param width `num` Proportion (not percent) of cell to fill with the bar (width wise). 
+#' @param fill `chr` CSS appropriate string indicating bar fill.
+#' @param color `chr` CSS appropriate sting indicating text color.
+#' @param font_weight `chr` Font weight.
+#' @param border_right `bool / chr` Border to be used on the right side of the cell. If `TRUE` a 
+#'   default border style of a thin grey line is used. The user can also pass a CSS style string.
+#'
+#' @export
 render_reactable_bar <- function(width, fill = "#e6e6e6", color = '#000000', font_weight = 'bold',
                                  border_right = NULL) {
   width <- paste0(width * 100, "%")
@@ -14,6 +26,15 @@ render_reactable_bar <- function(width, fill = "#e6e6e6", color = '#000000', fon
   return(out)
 }
 
+#' Reactable cell border
+#' 
+#' @description Convenience function to add a cell border to a reactable.
+#' 
+#' @param border `bool / chr` A user can either pass `TRUE` to add a default border (thin and grey)
+#'   or they can pass their own CSS style string. Anything else will result in no border. Default
+#'   is NULL.
+#'
+#' export
 render_reactable_border <- function(border = NULL) {
   out <- if (is.null(border)) {
            return(NULL)
@@ -27,6 +48,14 @@ render_reactable_border <- function(border = NULL) {
          }
 }
 
+#' Reactable headers (with tooltips)
+#'
+#' @description Convenience function to facilitate adding tooltips to reactable headers.
+#'
+#' @param name `chr` Column name.
+#' @param tooltip `chr` Tooltip to be added to the header text. Default is NULL (no tooltip).
+#' 
+#' @export
 render_reactable_header <- function(name, tooltip = NULL) {
   if (is.null(tooltip)) {
     out <- name
@@ -43,3 +72,4 @@ render_reactable_header <- function(name, tooltip = NULL) {
   
   return(out)
 }
+
